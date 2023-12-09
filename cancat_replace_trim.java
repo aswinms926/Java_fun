cb38c3e01b8acdcea92a902314049649ab81e1eb
Aswin M.S <125258048+aswinms926@users.noreply.github.com>
2023-12-09 07:02:24 +0530

    Add files via upload

---
+++ cancat_replace_trim.java
@@ -0,0 +1,14 @@
+package JAVA;
+
+public class cancat_replace_trim
+{
+    public static void main(String [] args)
+    {
+        String s="hai my name is ";
+        String s1="Aswin";
+        System.out.println("concat="+s.concat(s1));
+        System.out.println("replace="+s1.replace('A','a'));
+        System.out.println("trim="+s.trim());
+    }
+
+}
