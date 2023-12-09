6bcc18173d128e8ef1bbe53ef1c3dd301701c2b1
Aswin M.S <125258048+aswinms926@users.noreply.github.com>
2023-12-09 06:46:06 +0530

    Add files via upload

---
+++ LastindexofAND_indexOf.java
@@ -0,0 +1,13 @@
+package JAVA;
+
+public class LastindexofAND_indexOf
+{
+    public static void main ( String [] args)
+    {
+        String s1="Now is the time for all good men to come to the aid of their country";
+        System.out.println("b="+s1.lastIndexOf('t',60));
+        System.out.println("c="+s1.indexOf("the"));
+        String s2= "hai my name is Aswin";
+        System.out.println("s="+s2.substring(15));
+    }
+}
