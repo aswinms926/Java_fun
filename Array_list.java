03eee5a64dab86f304acb2ca966d747550c66026
Aswin M.S <125258048+aswinms926@users.noreply.github.com>
2024-01-14 11:41:20 +0530

    Add files via upload

--- Array_list.java
+++ Array_list.java
@@ -3,12 +3,23 @@ import java.util.*;
 public class Array_list {
     public static void main(String args[])
     {
-        ArrayList<String> n=new ArrayList<>();
-        n.add("java");
-        n.add("python");
-        n.add("shift");
+        ArrayList<Integer> n=new ArrayList<>(5);
+        n.add(1);
+        n.add(2);
+        n.add(3);
+
         System.out.println("Array LIst "+n);
-        n.add(0,"c");
-        System.out.println("Modified Array list : "+n);
+
+        ArrayList<Integer> m=new ArrayList<>(5);
+        m.add(5);
+        m.add(6);
+        m.add(7);
+
+
+            System.out.println("Array LIst "+m);
+
+        n.addAll(m);
+
+            System.out.println("Array LIst "+n);
     }
 }
